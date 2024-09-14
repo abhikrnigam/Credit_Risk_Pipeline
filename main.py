25d3ef27ae3dc697aa955eee9e2cf1fb31a277a3
Abhishek Kumar <abhikrnigam@gmail.com>
2024-09-14 22:58:43 +0530

    Merged files

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pandas as pd
 import preprocessing
 import model
+import uvicorn
+from fastapi import FastAPI
+
+#object
+app = FastAPI()
+
+#on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello,Users'}
 
 
+#on http://127.0.0.1:8000/xyz....
+@app.get('/Welcome')
+def get_name(name: str)
+    return{'Check your credit risk': f'{name}'}
+
+
+#run
 if __name__ == '__main__':
-
     preprocessing_object = preprocessing.Preprocessing()
     data = preprocessing_object.DataPreprocessing()
     model_object = model.Model()
     classification_model = model_object.predict(data)
     model_object.save_model(classification_model)
     print(model_object)
+    uvicorn.run(app, host = '127.0.0.1', port=8000)
 
 
-
-
+# uvicorn main:app --reload 
